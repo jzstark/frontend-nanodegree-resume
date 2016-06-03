d82e094f2539a0ee4cad17848386559a8a0e082c
J. Roger Zhao <rho.ajax@gmail.com>
2016-06-03 15:28:03 +0100

    Finish Course 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,35 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*
+var name = "Roger Stark";
+ var role  = "Lord of Winterfell";
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole  = HTMLheaderRole.replace("%data%", role);
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+*/
+
+var bio = {
+  "name" : "Roger Stark",
+  "role" : "Lord of Winterfell",
+  "contact" : {
+    "mobile" : "123-4567",
+    "email"  : "roger.stark@gmail.com",
+    "github" : "rogers",
+    "location": "SanFrancisco"
+  },
+  "welcomeMessage": "Valarmoghlis",
+  "skills" : ["javascript", "Python", "Greenseeing"],
+  "bioPic" : "images/fry.jpg"
+};
+
+var education = {};
+
+var work = {};
+
+var project = {};
